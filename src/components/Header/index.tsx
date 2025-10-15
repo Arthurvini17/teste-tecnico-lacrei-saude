82275791634bc26541a62b8cc0d3f0ca5c1066d3
arthur <arthurdedsec@gmail.com>
2025-10-15 15:29:35 -0300

    Add Header component and update dependencies
    - Introduced a new Header component for navigation.
    - Added "lucide-react" dependency for icons.
    - Updated package.json and package-lock.json to reflect new dependencies.
    - Cleaned up globals.css by removing unused CSS variables.
    - Simplified the Home component layout in page.tsx.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,98 @@
+"use client"
+
+import React, { useState } from "react";
+import { HeartHandshake, Menu, X } from "lucide-react";
+
+export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <header className="w-full bg-transparent">
+            <nav className="flex items-center justify-between h-16 px-6 md:px-12 relative">
+                {/* Logo - esquerda */}
+                <div className="inline-flex items-center gap-2 font-semibold text-[#018762] text-lg">
+                    <HeartHandshake />
+                    <p>Lacrei Saúde</p>
+                </div>
+
+                {/* Desktop */}
+                <ul className="hidden md:flex   font-bold items-center gap-8 text-sm  text-[#000000]">
+                    <li>
+                        <a href="#" className="hover:text-[#00A884] transition-colors">
+                            Início
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-[#00A884] transition-colors">
+                            Sobre
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:text-[#00A884] transition-colors">
+                            Contato
+                        </a>
+                    </li>
+                </ul>
+
+
+                <div className="hidden md:flex items-center gap-3">
+                    <button className="px-4 py-1 border shadow-sm border-[#018762] text-[#018762] font-semibold rounded-md hover:bg-[#018762] hover:text-white transition-colors">
+                        Entrar
+                    </button>
+                    <button className="px-4 py-1 border border-[#018762] text-[#018762] font-semibold rounded-md hover:bg-[#018762] hover:text-white transition-colors">
+                        Cadastrar
+                    </button>
+                </div>
+
+                {/* botão do mobile */}
+                <button
+                    className="md:hidden text-[#018762]"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    aria-label={menuOpen ? "Fechar menu" : "Abrir menu"}
+                    aria-expanded={menuOpen}
+                >
+                    {menuOpen ? <X size={28} /> : <Menu size={28} />}
+                </button>
+            </nav>
+
+            {/* menu mobile */}
+            {menuOpen && (
+                <div className="md:hidden flex flex-col items-center gap-6 py-8 bg-white shadow-lg border-t border-[#018762]/20 animate-fadeIn">
+                    <ul className="flex flex-col items-center w-full gap-4 pt-4 border-t border-[#018762]/10">
+                        <li>
+                            <a
+                                href="#"
+                                className="text-[#018762] text-base font-medium hover:text-[#00A884] transition-colors">
+                                Início
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="text-[#018762] text-base font-medium hover:text-[#00A884] transition-colors">
+                                Sobre
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="text-[#018762] text-base font-medium hover:text-[#00A884] transition-colors" >
+                                Contato
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div className="flex flex-col w-full items-center gap-4">
+                        <button className="w-10/12 py-2 border shadow-md text-[#018762] font-semibold rounded-md hover:bg-[#018762] hover:text-white transition-colors">
+                            Entrar
+                        </button>
+
+                        <button className="w-10/12 py-2 border bg-[#018762] text-white font-semibold rounded-md hover:bg-[#018762] hover:text-white transition-colors">
+                            Cadastrar
+                        </button>
+                    </div>
+                </div>
+            )}
+        </header>
+    );
+}
